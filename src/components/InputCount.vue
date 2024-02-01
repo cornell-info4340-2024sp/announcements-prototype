57d8aef37163f40018dc6f3db5e520cbde752fc1
Kyle J. Harms <harmsk@users.noreply.github.com>
2024-02-01 16:56:57 +0000

    Count example

---
+++ src/components/InputCount.vue
@@ -0,0 +1,4 @@
+<template>
+  <input />
+  <button v-on:click="$emit('increaseBy', 1)">Increase Count</button>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import InputCount from '@/components/InputCount.vue';
+import Count from '@/components/Count.vue';
+
+import { ref } from 'vue';
+
+const count = ref(0);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <InputCount v-on:increase-by="count++" />
+    <Count />
+    {{  count }}
   </main>
 </template>
